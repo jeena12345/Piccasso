084b662853f2a7a1d24a5b96993f04ed4e808710
jeena12345 <46891864+jeena12345@users.noreply.github.com>
2019-02-27 10:29:26 +0530

    Initial commit

---
+++ app/src/main/java/com/sruthy/piccasso/MainActivity.java
@@ -0,0 +1,27 @@
+package com.sruthy.piccasso;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import com.squareup.picasso.Picasso;
+public class MainActivity extends AppCompatActivity {
+ImageView im;
+Button bt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        im=findViewById(R.id.imageView);
+        bt=findViewById(R.id.button);
+       // Picasso.get().load("https://images.pexels.com/photos/257360/pexels-photo-257360.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500").into(im);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                im.setVisibility(View.VISIBLE);
+            Picasso.get().load("https://images.pexels.com/photos/257360/pexels-photo-257360.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500").into(im);
+            }
+        });
+    }
+}
